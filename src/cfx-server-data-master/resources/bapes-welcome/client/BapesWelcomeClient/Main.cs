f27b9ceb1261097f476d305a9d8fec20c6d72e7d
Bill Chirico <billchirico@gmail.com>
2020-07-02 20:23:04 -0400

    Add initial Welcomer

---
+++ src/cfx-server-data-master/resources/bapes-welcome/client/BapesWelcomeClient/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CitizenFX.Core;
+
+namespace BapesWelcomeClient
+{
+    public class Class1 : BaseScript
+    {
+        public Class1()
+        {
+            EventHandlers["playerSpawned"] += new Action<Player>(OnPlayerSpawn);
+        }
+
+        private static void OnPlayerSpawn(Player obj)
+        {
+            TriggerEvent("chatMessage", "SYSTEM", new[] {255, 0, 0}, "Welcome to the server!");
+        }
+    }
+}
---
+++ src/cfx-server-data-master/resources/bapes-welcome/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Bapes'
+description 'Player Welcome'
+version '1.0.0'
+
+-- client_script 'client.lua'
+server_script 'BapesWelcomeClient.net.dll'
